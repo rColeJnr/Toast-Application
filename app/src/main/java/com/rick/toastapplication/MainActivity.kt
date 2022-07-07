82485eff4ae933507c38759f1a81af5db5125daf
rcolejnr <ricardo.m.jnr@gmail.com>
2022-07-07 18:51:43 +0300

    задача выполнена

--- app/src/main/java/com/rick/toastapplication/MainActivity.kt
+++ app/src/main/java/com/rick/toastapplication/MainActivity.kt
@@ -1,42 +1,63 @@
 package com.rick.toastapplication
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MyViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewModel = ViewModelProvider(this)[MyViewModel::class.java]
-
-        val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
-        if (sharedPref.getInt(PREF_KEY, 1) == 3) {
-            Toast.makeText(this, getString(R.string.message), Toast.LENGTH_LONG).show()
-            sharedPref.edit()
-                .putInt(PREF_KEY, -1)
-                .apply()
-        } else if (sharedPref.getInt(PREF_KEY, 1) == 1) {
-            viewModel.count++
-            with(sharedPref.edit()) {
-                putInt(PREF_KEY, viewModel.count)
-                apply()
-            }
-
-        } else if (sharedPref.getInt(PREF_KEY, 1) == 2) {
-            viewModel.count = 3
-            sharedPref.edit()
-                .putInt(PREF_KEY, viewModel.count)
-                .apply()
-        }
-
+        viewModel = ViewModelProvider(this)[MyViewModel::class.java]
 
     }
 
+    override fun onResume() {
+        super.onResume()
 
+        if (viewModel.flag) {
+            val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
+
+            when (sharedPref.getInt(PREF_KEY, 1)) {
+                1 -> {
+                    viewModel.updateCount()
+                    with(sharedPref.edit()) {
+                        putInt(PREF_KEY, viewModel.count)
+                        apply()
+                    }
+
+                }
+                2 -> {
+                    viewModel.updateCount()
+                    viewModel.updateCount()
+                    sharedPref.edit()
+                        .putInt(PREF_KEY, viewModel.count)
+                        .apply()
+                }
+                3 -> {
+                    Toast.makeText(this, getString(R.string.message), Toast.LENGTH_LONG).show()
+                    sharedPref.edit()
+                        .putInt(PREF_KEY, -1)
+                        .apply()
+                }
+            }
+
+        }
+    }
+
+    /**
+     * Чтобы не обновить значение параметра count при повороте экрана
+     * */
+    override fun onPause() {
+        super.onPause()
+        viewModel.updateFlag()
+    }
 }
 
 const val PREF_KEY = "pref_key"
--- app/src/main/java/com/rick/toastapplication/MyViewModel.kt
+++ app/src/main/java/com/rick/toastapplication/MyViewModel.kt
@@ -5,4 +5,17 @@ import androidx.lifecycle.ViewModel
 class MyViewModel: ViewModel() {
 
     var count = 1
+        private set
+
+    var flag = true
+        private set
+
+    fun updateFlag() {
+        flag = false
+    }
+
+    fun updateCount() {
+        count++
+    }
+
 }
